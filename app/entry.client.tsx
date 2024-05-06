b379e5d37255326d879ba25308bfa85e5ae4e7a7
Shinya Hara <rabby.coffee.c5@gmail.com>
2024-05-06 22:18:12 +0900

    ✨ liff.init()してからremix展開

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -4,15 +4,18 @@
  * For more information, see https://remix.run/file-conventions/entry.client
  */
 
+import { liff } from "@line/liff";
 import { RemixBrowser } from "@remix-run/react";
 import { StrictMode, startTransition } from "react";
 import { hydrateRoot } from "react-dom/client";
 
-startTransition(() => {
-	hydrateRoot(
-		document,
-		<StrictMode>
-			<RemixBrowser />
-		</StrictMode>,
-	);
+liff.init({ liffId: window.ENV.LIFF_ID as string }).then(() => {
+	startTransition(() => {
+		hydrateRoot(
+			document,
+			<StrictMode>
+				<RemixBrowser />
+			</StrictMode>,
+		);
+	});
 });
--- app/root.tsx
+++ app/root.tsx
@@ -4,9 +4,21 @@ import {
 	Outlet,
 	Scripts,
 	ScrollRestoration,
+	json,
+	useLoaderData,
 } from "@remix-run/react";
 
+export const loader = async () => {
+	return json({
+		ENV: {
+			LIFF_ID: process.env.LIFF_ID,
+		},
+	});
+};
+
 export function Layout({ children }: { children: React.ReactNode }) {
+	const data = useLoaderData<typeof loader>();
+
 	return (
 		<html lang="en">
 			<head>
@@ -18,6 +30,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
 			<body>
 				{children}
 				<ScrollRestoration />
+				<script
+					// biome-ignore lint/security/noDangerouslySetInnerHtml: passing env var
+					dangerouslySetInnerHTML={{
+						__html: `window.ENV = ${JSON.stringify(data.ENV)};`,
+					}}
+				/>
 				<Scripts />
 			</body>
 		</html>
