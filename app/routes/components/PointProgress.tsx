a9bffbf67b58460c1c4308334074fcfc31e74334
Shinya Hara <rabby.coffee.c5@gmail.com>
2024-05-26 13:21:07 +0900

    💄 ポイント進捗UI実装

---
+++ app/routes/components/PointProgress.tsx
@@ -0,0 +1,31 @@
+import { CircularProgress, Image } from "@nextui-org/react";
+
+type Props = {
+	maxPoint: number;
+	point: number;
+	pictureUrl?: string;
+};
+
+export const PointProgress = (props: Props) => {
+	const { maxPoint, point, pictureUrl = "/logo.jpg" } = props;
+
+	return (
+		<div className="relative">
+			<Image
+				src={pictureUrl}
+				alt=""
+				width={52}
+				height={52}
+				className="absolute top-3.5 left-3.5 rounded-full"
+			/>
+			<CircularProgress
+				maxValue={maxPoint}
+				value={point}
+				color="primary"
+				classNames={{
+					svg: "h-20 w-20",
+				}}
+			/>
+		</div>
+	);
+};
