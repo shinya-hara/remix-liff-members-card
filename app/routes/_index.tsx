5d7687c3ad0da1ca07c6774a3e07ccad5dbf2fe0
Shinya Hara <rabby.coffee.c5@gmail.com>
2024-05-26 15:07:12 +0900

    ✨ 会員証表示できるように

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,6 +2,7 @@ import liff from "@line/liff";
 import { Button, Card, CardBody, Image } from "@nextui-org/react";
 import type { MetaFunction } from "@remix-run/node";
 import { json, useLoaderData } from "@remix-run/react";
+import { QRCode } from "~/components/QRCode";
 import { MyPoint } from "./components/MyPoint";
 import { PointProgress } from "./components/PointProgress";
 
@@ -21,6 +22,10 @@ export const clientLoader = async () => {
 
 export default function Index() {
 	const { profile } = useLoaderData<typeof clientLoader>();
+	const qrCodeValue = JSON.stringify({
+		userId: profile.userId,
+		shopId: new URLSearchParams(location.search).get("shopId"),
+	});
 
 	return (
 		<div>
@@ -28,12 +33,7 @@ export default function Index() {
 				<CardBody className="px-8 py-6">
 					<div className="flex flex-col gap-4">
 						<div className="flex justify-center">
-							<Image
-								width={100}
-								height={100}
-								alt="NextUI hero Image with delay"
-								src="https://app.requestly.io/delay/2000/https://placehold.jp/200x200.png?text=QR%20Code"
-							/>
+							<QRCode value={qrCodeValue} />
 						</div>
 						<div className="flex justify-between items-center">
 							<PointProgress
@@ -41,11 +41,7 @@ export default function Index() {
 								point={350}
 								pictureUrl={profile.pictureUrl}
 							/>
-							<MyPoint
-								maxPoint={500}
-								point={350}
-								updatedAt={new Date("2024-05-26")}
-							/>
+							<MyPoint maxPoint={500} point={350} updatedAt={new Date()} />
 						</div>
 						<div className="text-center">
 							<div className="mb-4 relative text-sm text-primary font-medium inline-block rounded-lg px-2 py-1 border border-primary before:content-[''] before:absolute before:-translate-x-1/2 before:left-1/2 before:top-full before:border-r before:border-primary before:w-2 before:h-3 before:bg-white before:skew-x-[-25deg]">
